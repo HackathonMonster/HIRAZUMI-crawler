e50f452605edad5605f70721277cc3104869a8d1
Orleika <Orleika.net@gmail.com>
2015-03-08 04:14:15 +0900

    Modified qiita crawler

--- crawler.js
+++ crawler.js
@@ -5,7 +5,7 @@ var fs = require('fs'),
   async = require('async'),
   client = require('cheerio-httpcli'),
   readline = require('readline'),
-  document = require("jsdom").jsdom();
+  document = require('jsdom').jsdom();
 
 var sleep = function(milliSeconds) {
   var startTime = new Date().getTime();
@@ -30,7 +30,7 @@ var filter_number = function(str) {
   if (isFinite(str)) {
     return str;
   }
-  return '';
+  return -1;
 };
 
 var filter_size = function(size) {
@@ -44,7 +44,7 @@ var filter_price = function(str) {
   if (str) {
     return str.replace(/\s|\\|,|￥/g, '');
   }
-  return '';
+  return -1;
 };
 
 var htmlUnescape = function(html) {
@@ -95,7 +95,6 @@ var fetchQiita = function() {
     });
 
   rl.on('line', function(line) {
-    sleep(2000);
     client.fetch(baseUrl + line.trim(), {}, function(err, $, res) {
       if (err) {
         console.log(err);
@@ -114,13 +113,14 @@ var fetchQiita = function() {
         });
         if (flg) {
           data = '{"title":"' + $('.itemsShowHeaderTitle_title').text() +
-            '","article":"' + $article + '","amazon":"[';
+            '","article":"' + htmlUnescape($article).replace(/[\n\r]/g, '').replace(/"/g, '\\"') + '","isbn":[';
           amazon.forEach(function(ele, idx, arr) {
-            data += '"' + ele + '",';
+            data += '"' + ele.replace(/.+\/(\d{10})\/.*/, "$1") + '",';
           });
-          data.slice(0, -1);
-          data += '},';
+          data = data.slice(0, -1);
+          data += '],';
           fs.appendFile('./qiita/article.json', data);
+          sleep(2000);
         }
       }
     });
@@ -128,14 +128,17 @@ var fetchQiita = function() {
   rl.resume();
 };
 
-// fs.appendFile('article.txt', '[');
-// fetchQiita(1);
-// fs.appendFile('article.txt', ']');
+fetchQiita(1);
 
 var fetchCalilList = function(page) {
+  // 入門
   // var baseUrl = 'https://calil.jp/category?c=%E3%82%B3%E3%83%B3%E3%83%94%E3%83%A5%E3%83%BC%E3%82%BF&sc=%E4%B8%80%E8%88%AC%E3%83%BB%E5%85%A5%E9%96%80%E6%9B%B8&page=1';
-  var baseUrl = 'https://calil.jp/category?c=%E3%82%B3%E3%83%B3%E3%83%94%E3%83%A5%E3%83%BC%E3%82%BF&sc=%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0';
-
+  // プログラミング
+  // var baseUrl = 'https://calil.jp/category?c=%E3%82%B3%E3%83%B3%E3%83%94%E3%83%A5%E3%83%BC%E3%82%BF&sc=%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0';
+  // モバイル
+  // var baseUrl = 'https://calil.jp/category?c=%E3%82%B3%E3%83%B3%E3%83%94%E3%83%A5%E3%83%BC%E3%82%BF&sc=%E3%83%A2%E3%83%90%E3%82%A4%E3%83%AB%E3%83%BBi%E3%83%A2%E3%83%BC%E3%83%89';
+  // web
+  var baseUrl = 'https://calil.jp/category?c=%E3%82%B3%E3%83%B3%E3%83%94%E3%83%A5%E3%83%BC%E3%82%BF&sc=%E3%82%A4%E3%83%B3%E3%82%BF%E3%83%BC%E3%83%8D%E3%83%83%E3%83%88%E3%83%BBWeb%E9%96%8B%E7%99%BA';
   console.log(page);
 
   client.fetch(baseUrl, {
@@ -178,27 +181,27 @@ var fetchCalil = function() {
           descriptionData;
 
         data = '{"title":"' + filter_text($('.book_title')).replace(/\s/g, '') +
-          '","author":"' + filter_text($('.book_authors a:first-child')).replace(/(.+)\s+著/, "$1") +
+          '","author":"' + filter_text($('.book_authors a:first-child')) +
           '","publisher":"' + filter_text($('[itemprop=publisher]')) +
           '","date":"' + filter_text($('[itemprop=datePublished]')).slice(1, -1) +
           '","isbn":"' + isbn +
-          '","page":"' + filter_number(description.replace(/.+:(\d+)ページ.+/, "$1")) +
-          '","size":"' + filter_size(description.replace(/.+\/([\d\.x]+cm)ISBN.+/, "$1")) +
-          '","saleUrl":"' + filter_str(amazonUrl) +
+          '","page":' + filter_number(description.replace(/.+:(\d+)ページ.+/, "$1")) +
+          ',"size":"' + filter_size(description.replace(/.+\/([\d\.x]+cm)ISBN.+/, "$1")) +
+          '","shopUrl":"' + filter_str(amazonUrl) +
           '","imageUrl":"' + $('.largeimage').attr('src');
         if (amazonUrl) {
           client.fetch(amazonUrl, {}, function(err, $, res) {
             if (err) {
               console.log(err);
             } else {
-              data += '","yen":"' +
+              data += '","yen":' +
                 filter_price($('span.a-size-medium.a-color-price.offer-price.a-text-normal').text());
               client.fetch('http://www.amazon.co.jp/gp/aw/d/' + isbn + '/ref=mw_dp_mpd?er=1', {}, function(err, $, res) {
                 if (err) {
                   console.log(err);
                 } else {
                   descriptionData = $('table[color=#000000]+font').first().text();
-                  data += '","description":"' + escape(htmlUnescape(descriptionData)).slice(4) +
+                  data += ',"description":"' + escape(htmlUnescape(descriptionData)).slice(4) +
                     '"},';
                   fs.appendFile('./calil/book.json', data);
                 }
@@ -212,4 +215,4 @@ var fetchCalil = function() {
   rl.resume();
 };
 
-fetchCalil();
+// fetchCalil();
