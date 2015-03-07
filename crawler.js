951766cae58820e99a6c4cc83e7b3f3638c00d9c
Orleika <Orleika.net@gmail.com>
2015-03-07 22:10:56 +0900

    add qiita crawler

---
+++ crawler.js
@@ -0,0 +1,77 @@
+/**
+ * Module dependencies.
+ */
+var fs = require('fs'),
+  async = require('async'),
+  client = require('cheerio-httpcli'),
+  readline = require('readline');
+
+var sleep = function(milliSeconds) {
+  var startTime = new Date().getTime();
+  while (new Date().getTime() < startTime + milliSeconds) {}
+};
+
+var fetchQiitaList = function(page) {
+  // var baseUrl = 'http://qiita.com/search?page=1&q=%E6%9C%AC&sort=rel&utf8=%E2%9C%93';
+  var baseUrl = 'http://qiita.com/search?page=1&q=%E6%9B%B8%E7%B1%8D&sort=rel&utf8=%E2%9C%93';
+
+  console.log(page);
+
+  client.fetch(baseUrl, {
+    page: page
+  }, function(err, $, res) {
+    var articleSize = $('.searchResult').length;
+
+    if (articleSize <= 0) {
+      return;
+    }
+    $('.searchResult_itemTitle > a').each(function() {
+      url = $(this).attr('href');
+      fs.appendFile('url1.txt', url + "\n");
+    });
+
+
+    if (10 <= articleSize)
+      fetchQiitaList(++page);
+  });
+};
+
+// fetchQiitaList(1);
+
+var fetchQiita = function() {
+  var baseUrl = 'http://qiita.com',
+    rs = fs.ReadStream('url.txt'),
+    rl = readline.createInterface({
+      'input': rs,
+      'output': {}
+    });
+
+  rl.on('line', function(line) {
+    client.fetch(baseUrl + line.trim(), {}, function(err, $, res) {
+      if (err) {
+        console.log(err);
+      } else {
+        var $article = $('[itemprop=articleBody]'),
+          flg = false,
+          amazon = [],
+          data;
+        console.log($('.itemsShowHeaderTitle_title').text());
+        $article.find('a').each(function() {
+          if ($(this).attr('href').match(/www\.amazon/)) {
+            amazon.push($(this).attr('href'));
+            flg = true;
+          }
+        });
+        // if (flg) {
+        //   data = '{"title":"' + $('.itemsShowHeaderTitle_title').text() +
+        //    '}';
+        // }
+      }
+    });
+  });
+  rl.resume();
+};
+
+fs.appendFile('article.txt', '[');
+fetchQiita(1);
+fs.appendFile('article.txt', ']');
